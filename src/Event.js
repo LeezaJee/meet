09353eae72d5268e4688cd250357345aea6e1f32
Mona Lisa <lisa.nadeem@outlook.de>
2022-07-24 15:12:53 +0200

    created Event.js file to add the actual events to render in EventList

---
+++ src/Event.js
@@ -0,0 +1,10 @@
+// contains the actual events that will be rendered in the EventList component
+
+import React, { Component } from "react";
+
+class Event extends Component {
+  render() {
+    return <div></div>;
+  }
+}
+export default Event;
