b57377407fad3890b582fc086e6f7cb08a5b101c
Mona Lisa <lisa.nadeem@outlook.de>
2022-07-24 17:31:55 +0200

    refactored test code by adding beforeAll function for AppWrapper

--- src/__tests__/app.test.js
+++ src/__tests__/app.test.js
@@ -5,8 +5,14 @@ import EventList from "../EventList";
 import CitySearch from "../CitySearch";
 
 describe("<App /> component", () => {
+  let AppWrapper;
+  // any code within a beforeAll() function will be executed before each and every one of the tests
+  beforeAll(() => {
+    AppWrapper = shallow(<App />);
+  });
+
+  // checks if an EventList component exists in <App/>
   test("render EventList component", () => {
-    const AppWrapper = shallow(<App />);
     expect(AppWrapper.find(EventList)).toHaveLength(1);
   });
 });
