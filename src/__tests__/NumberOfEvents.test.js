b64aebbc704cb5450100105266b1eb743af27f12
Mona Lisa <lisa.nadeem@outlook.de>
2022-07-26 00:31:53 +0200

    created NumberOfEvents test file with 6 tests

---
+++ src/__tests__/NumberOfEvents.test.js
@@ -0,0 +1,54 @@
+import React from "react";
+import { shallow } from "enzyme";
+import NumberOfEvents from "../NumberOfEvents";
+
+describe("<NumberofEvents /> component", () => {
+  let NumberOfEventsWrapper;
+  beforeAll(() => {
+    NumberOfEventsWrapper = shallow(<NumberOfEvents />);
+  });
+
+  //TEST 1
+  test("render number of events", () => {
+    expect(NumberOfEventsWrapper.find(".numberOfEvents")).toHaveLength(1);
+  });
+
+  // TEST 2
+  test("default number set at 32", () => {
+    expect(NumberOfEventsWrapper.find("#default").get(0).props.value).toEqual(
+      32
+    );
+  });
+
+  // TEST 3
+  test("input value above 0", () => {
+    NumberOfEventsWrapper.setState({ numberOfEvents: 32 });
+    NumberOfEventsWrapper.find(".default").simulate("change", {
+      target: { value: -10 },
+    });
+    expect(NumberOfEventsWrapper.state("numberOfEvents")).toEqual(32);
+  });
+
+  // TEST 4
+  test("render input form", () => {
+    expect(NumberOfEventsWrapper.find("#default")).toHaveLength(1);
+  });
+
+  // TEST 5
+  test("allow user to edit list total", () => {
+    NumberOfEventsWrapper.setState({ numberOfEvents: 32 });
+    NumberOfEventsWrapper.find(".default").simulate("change", {
+      target: { value: 12 },
+    });
+    expect(NumberOfEventsWrapper.state("numberOfEvents")).toEqual(12);
+  });
+
+  // TEST 6
+  test("allow only numbers", () => {
+    NumberOfEventsWrapper.setState({ numberOfEvents: 32 });
+    NumberOfEventsWrapper.find("#default").simulate("change", {
+      target: { value: "" },
+    });
+    expect(NumberOfEventsWrapper.state("numberOfEvents")).toEqual(32);
+  });
+});
