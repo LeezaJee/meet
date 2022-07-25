afd2d74d9b84b89cc143351365f657dbd98b00c4
Mona Lisa <lisa.nadeem@outlook.de>
2022-07-26 00:37:19 +0200

    added import statements and describe beforeAll function to Event.test.js

---
+++ src/__tests__/Event.test.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { shallow } from "enzyme";
+import Event from "../Event";
+import { mockData } from "../mock-data";
+
+describe("<Event /> component", () => {
+  let EventWrapper;
+  beforeAll(() => {
+    EventWrapper = shallow(<Event event={mockData[0]} />);
+  });
+
