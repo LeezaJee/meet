810bada96960b2cea7d77c476773da29ebb29db8
Mona Lisa <lisa.nadeem@outlook.de>
2022-07-25 11:33:40 +0200

    added new test "render text input correctly"

--- src/__tests__/CitySearch.test.js
+++ src/__tests__/CitySearch.test.js
@@ -17,4 +17,14 @@ describe("<CitySearch /> component", () => {
     const CitySearchWrapper = shallow(<CitySearch />);
     expect(CitySearchWrapper.find(".suggestions")).toHaveLength(1);
   });
+
+  test("render text input correctly", () => {
+    // setting const to the query from the CitySearch state (what the user types into the textbox)
+    const query = CitySearchWrapper.state("query");
+    // compares the value prop of each element that has the class "city" found within the CitySearch component
+    // (which is the input field you added in the CitySearch component)
+    // and checks if the input field's value prop is equal to what’s in the CitySearch query state
+    expect(CitySearchWrapper.find(".city").prop("value")).toBe(query);
+  });
+
 });
