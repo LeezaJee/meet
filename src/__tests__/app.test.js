11b4bd24f77e19ecf6ae03af3b78b02766c59fe0
Mona Lisa <lisa.nadeem@outlook.de>
2022-07-27 13:06:47 +0200

    imported mount, mockData, extractLocations and getEvents

--- src/__tests__/app.test.js
+++ src/__tests__/app.test.js
@@ -1,9 +1,11 @@
 import React from "react";
-import { shallow } from "enzyme";
+import { shallow, mount } from "enzyme";
 import App from "../App";
 import EventList from "../EventList";
 import CitySearch from "../CitySearch";
 import NumberOfEvents from "../NumberOfEvents";
+import { mockData } from "../mock-data";
+import { extractLocations, getEvents } from "../api";
 
 describe("<App /> component", () => {
   let AppWrapper;
