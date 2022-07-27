4402e765f08a5cea00ffb7fee39ff6ea2d58c013
Mona Lisa <lisa.nadeem@outlook.de>
2022-07-27 13:00:02 +0200

    imported mockData and added function to return mockData

--- src/api.js
+++ src/api.js
@@ -1,8 +1,14 @@
 // handles API calls
-// temporarily storing this function
+// temporarily storing functions
+import { mockData } from "./mock-data";
+
 // extracts event locations out of an array of events and removes duplicates
 export const extractLocations = (events) => {
   var extractLocations = events.map((event) => event.location);
   var locations = [...new Set(extractLocations)];
   return locations;
+};
+
+export const getEvents = async () => {
+  return mockData;
 };
