fcc70c5821100a5a4139f3b624a9dcb74abf71ed
Mona Lisa <lisa.nadeem@outlook.de>
2022-07-27 13:01:49 +0200

    imported getEvents, extractLocations from ./api

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import "./App.css";
 import EventList from "./EventList";
 import CitySearch from "./CitySearch";
 import NumberOfEvents from "./NumberOfEvents";
+import { getEvents, extractLocations } from "./api";
 
 class App extends Component {
   render() {
