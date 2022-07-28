459e74be697c3196c6c68e28e20e43fd3bdb7b2e
Mona Lisa <lisa.nadeem@outlook.de>
2022-07-28 10:33:03 +0200

    rearranged authorization code in api.js

--- src/api.js
+++ src/api.js
@@ -11,7 +11,9 @@ export const extractLocations = (events) => {
   return locations;
 };
 
-// takes the accessToken that has been found in localStorage and checks whether it’s valid or not
+// -------- AUTHORIZATION AND ACCESS TOKEN CODE ---------
+
+// if existing token was found in localStorage, it checks whether token is valid or not
 // if it’s not, then user is sent to the Google Authorization screen
 const checkToken = async (accessToken) => {
   const result = await fetch(
@@ -21,50 +23,6 @@ const checkToken = async (accessToken) => {
     .catch((error) => error.json());
 
   return result;
-};
-
-// checks whether there’s a path, then builds the URL with the current path
-// (or builds the URL without a path using window.history.pushState()
-const removeQuery = () => {
-  if (window.history.pushState && window.location.pathname) {
-    var newurl =
-      window.location.protocol +
-      "//" +
-      window.location.host +
-      window.location.pathname;
-    window.history.pushState("", "", newurl);
-  } else {
-    newurl = window.location.protocol + "//" + window.location.host;
-    window.history.pushState("", "", newurl);
-  }
-};
-
-// if you’re using localhost, you return the mock data; otherwise, you use the real API
-export const getEvents = async () => {
-  NProgress.start();
-
-  if (window.location.href.startsWith("http://localhost")) {
-    NProgress.done();
-    return mockData;
-  }
-
-  const token = await getAccessToken();
-
-  if (token) {
-    removeQuery(); // will remove the code from the URL once you’re finished with it
-    const url =
-      "https://v6n6jwhi1a.execute-api.eu-central-1.amazonaws.com/dev/api/get-events" +
-      "/" +
-      token;
-    const result = await axios.get(url);
-    if (result.data) {
-      var locations = extractLocations(result.data.events);
-      localStorage.setItem("lastEvents", JSON.stringify(result.data));
-      localStorage.setItem("locations", JSON.stringify(locations));
-    }
-    NProgress.done();
-    return result.data.events;
-  }
 };
 
 // checks whether an access token was found
@@ -89,4 +47,71 @@ export const getAccessToken = async () => {
     return code && getToken(code);
   }
   return accessToken;
+};
+
+// takes your code and encodes it using encodeURIComponent, then uses the encoded code to get your token
+const getToken = async (code) => {
+  const encodeCode = encodeURIComponent(code);
+  const { access_token } = await fetch(
+    "https://v6n6jwhi1a.execute-api.eu-central-1.amazonaws.com/dev/api/token" +
+      "/" +
+      encodeCode
+  )
+    .then((res) => {
+      return res.json();
+    })
+    .catch((error) => error);
+
+  access_token && localStorage.setItem("access_token", access_token);
+
+  return access_token;
+};
+
+// removes the code from the URL once you’re finished with it.
+// checks whether there’s a path, then builds the URL with the current path
+// (or builds the URL without a path using window.history.pushState()
+const removeQuery = () => {
+  if (window.history.pushState && window.location.pathname) {
+    var newurl =
+      window.location.protocol +
+      "//" +
+      window.location.host +
+      window.location.pathname;
+    window.history.pushState("", "", newurl);
+  } else {
+    newurl = window.location.protocol + "//" + window.location.host;
+    window.history.pushState("", "", newurl);
+  }
+};
+
+// -------- getEvent CODE ---------
+
+// if you’re using localhost, you return the mock data; otherwise, you use the real API
+// NProgress package is used to create and display progress bars at the top of the page
+// this way, you can show users that the application is loading data when it tries to access the API
+export const getEvents = async () => {
+  NProgress.start();
+
+  if (window.location.href.startsWith("http://localhost")) {
+    NProgress.done();
+    return mockData;
+  }
+
+  const token = await getAccessToken(); // checks for an access token
+
+  if (token) {
+    removeQuery(); // will remove the code from the URL once you’re finished with it
+    const url =
+      "https://v6n6jwhi1a.execute-api.eu-central-1.amazonaws.com/dev/api/get-events" +
+      "/" +
+      token;
+    const result = await axios.get(url);
+    if (result.data) {
+      var locations = extractLocations(result.data.events);
+      localStorage.setItem("lastEvents", JSON.stringify(result.data));
+      localStorage.setItem("locations", JSON.stringify(locations));
+    }
+    NProgress.done();
+    return result.data.events;
+  }
 };
