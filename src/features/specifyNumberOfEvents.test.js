3e5022d285e7046ce3e8c182f137595fc86fae46
Mona Lisa <lisa.nadeem@outlook.de>
2022-08-03 15:23:02 +0200

    defined feature and added step definition teest for first scenario

--- src/features/specifyNumberOfEvents.test.js
+++ src/features/specifyNumberOfEvents.test.js
@@ -9,3 +9,31 @@ import NumberOfEvents from "../NumberOfEvents";
 const feature = loadFeature("./src/features/specifyNumberOfEvents.feature");
 const locations = extractLocations(mockData);
 
+defineFeature(feature, (test) => {
+  <></>;
+
+  let AppWrapper;
+  let NumberOfEventsWrapper;
+
+  // Scenario 1
+  test("When the user hasn’t specified a number, 32 is the default number.", ({
+    given,
+    when,
+    then,
+  }) => {
+    given("the user at the home page", async () => {
+      AppWrapper = await mount(<App />);
+    });
+
+    when("the user chooses no specific amount of search results", () => {
+      AppWrapper.update();
+    });
+
+    then(
+      "the default amount of visible search results per city will be 32",
+      () => {
+        expect(AppWrapper.find(".event-visible")).toHaveLength(2);
+      }
+    );
+  });
+
