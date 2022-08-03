b10bab4681e35c27946fdc85afac81310d54a2c8
Mona Lisa <lisa.nadeem@outlook.de>
2022-08-03 15:10:54 +0200

    defined feature and added step definition test for first scenario

--- src/features/showHideAnEventsDetails.test.js
+++ src/features/showHideAnEventsDetails.test.js
@@ -5,4 +5,24 @@ import App from "../App";
 
 const feature = loadFeature("./src/features/showHideAnEventsDetails.feature");
 
-defineFeature(feature, (test) => {});
+defineFeature(feature, (test) => {
+  <></>;
+  let AppWrapper;
+
+  // Scenario 1
+  test("An event element is collapsed by default.", ({ given, when, then }) => {
+    given("that the user opens the main page", () => {
+      AppWrapper = mount(<App />);
+    });
+
+    when("the user views the event element of a city", () => {});
+
+    then(
+      "the event element from each city will initially be collapsed/hidden from the user",
+      () => {
+        AppWrapper.update();
+        expect(AppWrapper.find(".event-details")).toHaveLength(0);
+      }
+    );
+  });
+
