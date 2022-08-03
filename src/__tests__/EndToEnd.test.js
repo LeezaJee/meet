1a4aaa4832863a4afaa010e2d88425fdc663e940
Mona Lisa <lisa.nadeem@outlook.de>
2022-08-03 14:33:01 +0200

    added beforeAll() and afterAll() to avoid redundant code that applies to every test

--- src/__tests__/EndToEnd.test.js
+++ src/__tests__/EndToEnd.test.js
@@ -1,3 +1,19 @@
 import puppeteer from "puppeteer";
 
 describe("show/hide event details", () => {
+  let browser;
+  let page;
+
+  beforeAll(async () => {
+    jest.setTimeout(60000);
+    browser = await puppeteer.launch();
+
+    page = await browser.newPage();
+    await page.goto("http://localhost:3000/meet");
+    await page.waitForSelector(".event-visible");
+  });
+
+  afterAll(() => {
+    browser.close();
+  });
+
