4b9da64009a9d7196ebdfc660f3bd8f583251dea
Mona Lisa <lisa.nadeem@outlook.de>
2022-08-18 01:38:28 +0200

    changed heading text of scatter chart

--- src/App.js
+++ src/App.js
@@ -140,7 +140,7 @@ class App extends Component {
           />
         </Container>
 
-        <h2>Events in each city:</h2>
+        <h2>Number of Events in each city:</h2>
 
         <ResponsiveContainer height={400}>
           <ScatterChart margin={{ top: 20, right: 20, bottom: 20, left: 20 }}>
