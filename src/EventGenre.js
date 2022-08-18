792f2cf89c94c3685bd7e08ed5f9ce5cbb07a398
Mona Lisa <lisa.nadeem@outlook.de>
2022-08-18 15:08:27 +0200

    added a PieChart in a ResponsiveContainer

--- src/EventGenre.js
+++ src/EventGenre.js
@@ -1,3 +1,24 @@
 import React, { useEffect, useState } from "react";
 import { PieChart, Pie, ResponsiveContainer } from "recharts";
 
+  return (
+    <ResponsiveContainer height={400}>
+      <PieChart width={400} height={400}>
+        <Pie
+          data={data}
+          cx={200}
+          cy={200}
+          labelLine={false}
+          label={({ name, percent }) =>
+            `${name} ${(percent * 100).toFixed(0)}%`
+          }
+          outerRadius={80}
+          fill="#8884d8"
+          dataKey="value"
+        ></Pie>
+      </PieChart>
+    </ResponsiveContainer>
+  );
+};
+
+export default EventGenre;
