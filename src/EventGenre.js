42a327400e117252f719b41c502485ebd8b0691b
Mona Lisa <lisa.nadeem@outlook.de>
2022-08-18 15:30:14 +0200

    added condition to label property

--- src/EventGenre.js
+++ src/EventGenre.js
@@ -32,9 +32,11 @@ const EventGenre = ({ events }) => {
           cx={200}
           cy={200}
           labelLine={false}
-          label={({ name, percent }) =>
-            `${name} ${(percent * 100).toFixed(0)}%`
-          }
+          label={({ name, percent }) => {
+            if (percent > 0) {
+              return `${name} ${(percent * 100).toFixed(0)}%`;
+            }
+          }}
           outerRadius={80}
           fill="#8884d8"
           dataKey="value"
